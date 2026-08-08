93bd834a429bd0a3cadda5de5d598a32f8e80eca
AdrianRCV <adriancamposvargas4@gmail.com>
2026-08-08 18:32:56 +0200

    feat: wire Postgres migrations and repository into Main

--- src/main/scala/com/adrian/eventmetrics/Main.scala
+++ src/main/scala/com/adrian/eventmetrics/Main.scala
@@ -1,29 +1,38 @@
 package com.adrian.eventmetrics
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
 import com.comcast.ip4s.*
+import org.typelevel.doobie.hikari.HikariTransactor
+import org.typelevel.doobie.util.ExecutionContexts
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.*
-import org.typelevel.doobie.Transactor
 import com.adrian.eventmetrics.application.http.HttpApi
-import com.adrian.eventmetrics.infrastructure.db.DatabaseConfig
+import com.adrian.eventmetrics.infrastructure.db.{DatabaseConfig, Migrations}
 import com.adrian.eventmetrics.infrastructure.persistence.DoobieEventRepository
 
 object Main extends IOApp:
 
+  private def transactor(config: DatabaseConfig): Resource[IO, HikariTransactor[IO]] =
+    for
+      connectEC <- ExecutionContexts.fixedThreadPool[IO](32)
+      xa        <- HikariTransactor.newHikariTransactor[IO](
+                     driverClassName = "org.postgresql.Driver",
+                     url = config.jdbcUrl,
+                     user = config.user,
+                     pass = config.password,
+                     connectEC = connectEC
+                   )
+    yield xa
+
   def run(args: List[String]): IO[ExitCode] =
     for
       config <- DatabaseConfig.fromEnv[IO]
-      xa = Transactor.fromDriverManager[IO](
-        driver = "org.postgresql.Driver",
-        url = config.jdbcUrl,
-        user = config.user,
-        password = config.password,
-        logHandler = None
-      )
-      repo      = new DoobieEventRepository[IO](xa)
-      app       = HttpApi.routes[IO](repo).orNotFound
-      exitCode <- EmberServerBuilder
+      _      <- Migrations.migrate[IO](config)
+      exit   <- transactor(config).use { xa =>
+                  val repo = new DoobieEventRepository[IO](xa)
+                  val app  = HttpApi.routes[IO](repo).orNotFound
+
+                  EmberServerBuilder
                     .default[IO]
                     .withHost(host"0.0.0.0")
                     .withPort(port"8080")
@@ -31,4 +40,5 @@ object Main extends IOApp:
                     .build
                     .use(_ => IO.never)
                     .as(ExitCode.Success)
-    yield exitCode
+                }
+    yield exit
